175e468a4a7f4a9fcc487a00ecc2096e553c47c3
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-12-04 19:34:27 +0000

    Use lombok instead of POKO boilerplate

--- src/main/kotlin/com/kotlin/example/booklibraryservice/dto/Book.kt
+++ src/main/kotlin/com/kotlin/example/booklibraryservice/dto/Book.kt
@@ -1,35 +1,17 @@
 package com.kotlin.example.booklibraryservice.dto
 
+import lombok.Data
+import lombok.EqualsAndHashCode
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.MongoId
 
 @Document(collection = "books")
+@Data
+@EqualsAndHashCode
 class Book(
         @MongoId
         val isbn: String,
         val title: String,
         val author: Author,
         val yearPublished: Long
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Book
-
-        if (isbn != other.isbn) return false
-        if (title != other.title) return false
-        if (author != other.author) return false
-        if (yearPublished != other.yearPublished) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = isbn.hashCode()
-        result = 31 * result + title.hashCode()
-        result = 31 * result + author.hashCode()
-        result = 31 * result + yearPublished.hashCode()
-        return result
-    }
-}
+)
